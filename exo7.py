4c23663447c035e02d30ea2bf7c2bb437aff4e48
Adjy Sedar Desir <desiradjysedar@gmail.com>
2021-09-30 16:53:52 +0200

    1

--- exo7.py
+++ exo7.py
@@ -1,4 +1,6 @@
 annee=int(input("Entrer l'annee:"))
+"Une année est bissextile si elle est multiple de 4 sans être multiple de 100 ou si elle est
+multiple de 400, si non, elle n'est pas bissextile"
 
 if annee%4 == 0 and annee%100 != 0 or annee%400 == 0:
     print("{} est bissextile".format(annee))
